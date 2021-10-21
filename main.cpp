b5f814a905351d14e16f3146fa4cb04c51dce83e
Max Lyfoore <lyfoore@gmail.com>
2021-10-22 00:14:30 +0700

    22.10

--- main.cpp
+++ main.cpp
@@ -3,48 +3,71 @@
 #include <algorithm>
 #include <cmath>
 
-const double PRESIZE = 0.01;
+const double PRESIZE = 0.01 / 100; //0.01%
 double SIZE;
 
-class Point {
+class Point
+{
 public:
     double m_x;
     double m_y;
     double m_z;
     int number;
-    double getDistFromInit() {
-        return sqrt(m_x*m_x + m_y*m_y + m_z*m_z);
+    double getDistFromInit()
+    {
+        return sqrt(m_x * m_x + m_y * m_y + m_z * m_z);
     };
 };
 
-bool comparison(Point a, Point b) {
+bool comparison(Point a, Point b)
+{
     return (a.getDistFromInit() < b.getDistFromInit());
 }
 
-double difference(Point a, Point b) {
-    return sqrt(pow(b.m_x - a.m_x, 2) + 
+double difference(Point a, Point b)
+{
+    return sqrt(pow(b.m_x - a.m_x, 2) +
                 pow(b.m_y - a.m_y, 2) +
                 pow(b.m_z - a.m_z, 2));
 }
 
-int main() {
-    Point a1 {1, 1, 1, 1};
-    Point a2 {10, 4, 3, 2};
-    Point a3 {9, 5, 2, 3};
-    Point a4 {0, 4, 5, 4};
-    Point a5 {10, 0, 3, 5};
+std::vector<Point> merging(std::vector<Point> vector)
+{
+    for (int i = 0; i < vector.size(); i++)
+    {
+        for (int j = i; j < vector.size(); j++)
+        {
+            if (difference(vector[i], vector[j]) < SIZE * PRESIZE)
+            {
+                
+            }
+        }
+    }
+}
+
+int main()
+{
+    Point a1{1, 1, 1, 1};
+    Point a2{10, 4, 3, 2};
+    Point a3{9, 5, 2, 3};
+    Point b1{0, 4, 5, 4};
+    Point b2{10, 0, 3, 5};
+    Point b3{6, 4, 6, 6};
     int n = 5;
-    std::vector<Point> vec = {a1, a2, a3, a4, a5};
-    for (auto now : vec) {
+    std::vector<Point> vec = {a1, a2, a3, b1, b2, b3};
+    for (auto now : vec)
+    {
         std::cout << now.number << " ";
     }
     std::cout << std::endl;
     std::sort(vec.begin(), vec.end(), comparison);
-    for (auto now : vec) {
+    for (auto now : vec)
+    {
         std::cout << now.number << " ";
     }
     std::cout << std::endl;
-    for (auto now : vec) {
+    for (auto now : vec)
+    {
         std::cout << now.getDistFromInit() << " ";
     }
     SIZE = vec[vec.size() - 1].getDistFromInit() * 2;
